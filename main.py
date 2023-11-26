1995f7966a410f805390eeb649a043077f584cf6
nicollasgaribaldi <nicollasgaribaldi@hotmail.com>
2023-11-26 15:47:21 -0300

    variavel

--- main.py
+++ main.py
@@ -97,8 +97,8 @@ def form_cadastro_tarefa():
 def processar_tarefa():
     if request.method == 'POST':
         # Obtém os dados do formulário
-        titulo_receita = request.form.get('titulo_receita')
-        ingredientes_receita = request.form.get('ingredientes_receita')
+        titulo_tarefa = request.form.get('titulo_tarefa')
+        palavras_tarefa= request.form.get('palavras_tarefa')
 
         # Caminho para o arquivo CSV
         cadastro_tarefa = 'bd_glossario.csv'
